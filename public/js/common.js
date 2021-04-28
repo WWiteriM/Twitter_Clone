d58935f11c58bf9719f2f710cbf1070f153094a1
Arsenii <arsenii.mingazov@belstu.by>
2021-04-28 12:36:29 +0300

    Styled posts component

--- public/js/common.js
+++ public/js/common.js
@@ -36,5 +36,42 @@ $('#submitPostButton').click((event) => {
 });
 
 function createPostHtml(postData) {
-  return postData.content;
+  const { postedBy } = postData;
+  const displayName = `${postedBy.firstName} ${postedBy.lastName}`;
+  const timestamp = postData.createdAt;
+
+  return `<div class='post'>
+            <div class='mainContentContainer'>
+                <div class='userImageContainer'>
+                    <img src='${postedBy.profilePic}'>
+                </div>
+                <div class='postContentContainer'>
+                    <div class='header'>
+                        <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
+                        <span class='username'>@${postedBy.username}</span>
+                        <span class='date'>${timestamp}</span>
+                    </div>
+                    <div class='postBody'>
+                        <span>${postData.content}</span>
+                    </div>
+                    <div class='postFooter'>
+                        <div class='postButtonContainer'>
+                            <button>
+                                <i class='far fa-comment'></i>
+                            </button>
+                        </div>
+                        <div class='postButtonContainer'>
+                            <button>
+                                <i class='fas fa-retweet'></i>
+                            </button>
+                        </div>
+                        <div class='postButtonContainer'>
+                            <button>
+                                <i class='far fa-heart'></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>`;
 }
