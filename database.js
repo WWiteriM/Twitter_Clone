627a5f1d94a39d2df328ca04087a6e3038824186
Arsenii <arsenii.mingazov@belstu.by>
2021-05-13 20:58:03 +0300

    Initial commit on heroku

--- database.js
+++ database.js
@@ -16,11 +16,9 @@ class Database {
         'mongodb+srv://admin:12345@twitterclonecluster.trl59.mongodb.net/TwitterDB?retryWrites=true&w=majority',
       )
       .then(() => {
-        // eslint-disable-next-line no-console
         console.log('Success');
       })
       .catch((err) => {
-        // eslint-disable-next-line no-console
         console.log(`MongoDb connection error -- ${err}`);
       });
   }
